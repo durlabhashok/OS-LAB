10c1608777305af502ffcd84d612a9d98df9dfab
durlabhashok <55022250+durlabhashok@users.noreply.github.com>
2019-09-07 17:35:56 +0500

    lab1

---
+++ Lab1/HelloWorldC.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+int fno, sno;
+printf("Enter 1st Number \n");
+scanf("%d",&fno);
+
+printf("Enter 2nd Number \n");
+scanf("%d",&sno);
+
+printf("Your Number is,%d \n",fno+sno );
+return 0;
+}
